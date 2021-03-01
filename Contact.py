62ad98aaeda86dfa56d0ab8b89edec8c47a5f9a1
mkhoury-pc <matthewkhoury9@gmail.com>
2021-02-28 22:05:41 -0500

    Added Contact and Commands classes

---
+++ Contact.py
@@ -0,0 +1,8 @@
+#Class for the contact
+class Contact:
+    def __init__(self, name, relation, telephone_number):
+        self.name = name
+        self.relation = relation
+        self.telephone_number = telephone_number
+
+        contact_dictionary [name] = {"relation ":relation, "telephone number ":telephone_number}
--- address_book.py
+++ address_book.py
@@ -1,5 +1,11 @@
+from Contact import Contact 
+from Commands import Commands
 
 
+#Dictionary to store contact information
+contact_dictionary = {"name":{"relationship":"", "telephone_number":""}}
+#List of relation types 
+relation_type = ["family", "friend", "coworker"]
 
 
 
