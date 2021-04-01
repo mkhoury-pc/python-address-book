69b581b4fcacda80cbe4a8c8266939743e25b582
mkhoury-pc <matthewkhoury9@gmail.com>
2021-04-01 15:19:18 -0400

    Tweaked the file system, still having issues with empty dictionary string in json file

--- address_book.py
+++ address_book.py
@@ -3,47 +3,50 @@ import json
 import time
 
 #Dictionary to store contact information
-contact_dictionary = {}
+contact_dict = {}
 
 #Assigning the address book data to a file to save dictionary data
-address_book_data = "address_book_data.json"
+if os.path.exists("contacts.json"):
+    with open("contacts.json", "r") as f:
+        contact_dict = json.load(f)
+else:
+    with open("contacts.json", "w") as f:
+        json.dump(contact_dict, f)
 
 #Function to add a contact
 def add_contact():
     input_name = input("Enter the contact's name:  ")
     input_telephone_number = input("Enter the contact's phone number:  ")
-    contact_dictionary[input_name] = input_telephone_number
+    contact_dict[input_name] = input_telephone_number
     print("Added " + input_name + " with telephone number " + input_telephone_number)
-    with open(address_book_data, 'a') as f:
-        f.write(json.dumps(contact_dictionary))
+    with open("contacts.json", 'a') as f:
+        json.dump(contact_dict, f)
     time.sleep(1)
     
 
 #Function to delete a contact
 def del_contact():
     contact = input("Enter the name of the contact to delete:  ")
-    if contact in contact_dictionary:
-        del contact_dictionary[contact]
+    if contact in contact_dict:
+        del contact_dict[contact]
         print("Deleted " + contact)
     else:
         print("Contact does not exist.")
         time.sleep(1)
 
 
-#Function to search for a contact
-def search():
-    name = input("Enter the name of the contact you are looking for:  ")
-    if name in contact_dictionary:
-        print("Name: %s, Phone: %s"%(name, contact_dictionary [name], contact_dictionary [name] ["telephone_number"]))
-    else:
-        print("Contact does not exist.")
+#Function to display contacts
+def list_contacts():
+    print(contact_dict)
+    time.sleep(1.2)
+
 
 #Master Loop for Running the program
 while True:
     print("Welcome to your address book.")
     print("Enter '0' to add a contact.")
     print("Enter '1' to delete a contact.")
-    print("Enter '2' to search for a contact.")
+    print("Enter '2' to display all contacts.")
     print("Enter '3' to quit")
     response = input("What would you like to do?  ")
     #Make sure the user's input is an integer
@@ -63,9 +66,9 @@ while True:
     elif response == 1:
         del_contact()
         
-    #Response to search for a contact
+    #Response to show all contacts
     elif response == 2:
-        search()
+        list_contacts()
         
     #Response to quit program
     elif response == 3:
